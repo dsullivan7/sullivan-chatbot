13f969e85ff04a338db136e74ebef77e6ca2df31
dsullivan7 <dbsullivan23@gmail.com>
2017-06-20 10:34:13 +1000

    updating to new flow configuration

--- bot/flow.js
+++ bot/flow.js
@@ -6,13 +6,13 @@ const offscript = user => (!user.responses[user.currentState].payload ||
 
 // flow object represents the flow through a conversation
 const flow = {
-  initialState: states.GET_STARTED,
   states: {
     [states.GET_STARTED]: {
-      next: () => states.GREETING,
+      match: (user, messageData) => (messageData.payload === 'GET_STARTED' || messageData.text === 'Get Started'),
+      next: states.GREETING,
     },
     [states.GREETING]: {
-      next: () => states.QUICK_REPLY_DEMO,
+      next: states.QUICK_REPLY_DEMO,
       message: user => ({
         type: 'text',
         text: `Hey there ${userStore[user.userId].profile.first_name}! Thanks for the message!`,
@@ -21,19 +21,19 @@ const flow = {
     },
     [states.QUICK_REPLY_DEMO]: {
       next: user => (offscript(user) ? states.OFF_SCRIPT : states.POSTBACK_DEMO),
-      message: () => ({
+      message: {
         type: 'quickReply',
         text: 'We can show buttons with different options, as you can see below.',
         answers: {
           [`${states.QUICK_REPLY_DEMO}_OPTION_1`]: 'Option 1',
           [`${states.QUICK_REPLY_DEMO}_OPTION_2`]: 'Option 2',
         },
-      }),
+      },
     },
     [states.POSTBACK_DEMO]: {
       next: user => (offscript(user) ? states.OFF_SCRIPT : states.QUESTION),
       messageType: 'postback',
-      message: () => ({
+      message: {
         type: 'postback',
         text: 'We can also show a different type of button.',
         answers:
@@ -41,30 +41,30 @@ const flow = {
             [`${states.POSTBACK_DEMO}_OPTION_1`]: 'Option 1',
             [`${states.POSTBACK_DEMO}_OPTION_2`]: 'Option 2',
           }),
-      }),
+      },
     },
     [states.OFF_SCRIPT]: {
-      message: () => ({
+      message: {
         type: 'text',
         text: 'No problem, we\'ll have someone reach out to you shortly',
-      }),
+      },
       noReply: true,
-      next: () => null,
+      next: null,
     },
     [states.QUESTION]: {
-      next: () => states.CLOSING,
-      message: () => ({
+      next: states.CLOSING,
+      message: {
         type: 'text',
         text: 'Cool, now is there a specific question you\'re wondering about?',
-      }),
+      },
     },
     [states.CLOSING]: {
-      message: () => ({
+      message: {
         type: 'text',
         text: 'Alrighty, we\'ll forward your info to the right person. Thanks very much for getting in touch with us. Have a great day and we’ll be in touch shortly!',
-      }),
+      },
       noReply: true,
-      next: () => null,
+      next: null,
     },
   },
 }
